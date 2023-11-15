ef372e5d0058014b7399b0785d89d3d98da7c728
Apocalopsys <onuh.charles@lmu.edu.ng>
2023-11-15 07:56:22 -0800

    PyCode

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def uppercase(str):
-    leng = len(str)
+def uppercase(some_str):
+    leng = len(some_str)
     for z in range(0, leng):
-        x = str[z]
-        if ord(x) <= ord('z') and ord(z) >= ord('a'):
+        x = some_str[z]
+        if ord(x) >= ord('a') and ord(x) <= ord('z'):
             x = chr(ord(x) - 32)
         print('{}'.format(x), end='')
     print()
